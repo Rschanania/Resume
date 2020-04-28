405c7d483594af4de2bd8f1baf706d916f1b4a88
Rschanania <chananias1@gmail.com>
2020-04-28 20:16:45 +0530

    Django-herok

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,3 +131,6 @@ os.makedirs(STATIC_ROOT, exist_ok=True)
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles')
 ]
+
+
+django_heroku.settings(locals())
